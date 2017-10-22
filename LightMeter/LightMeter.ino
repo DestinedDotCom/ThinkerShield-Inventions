a02553ff3cdae4e39104fc0cf07f375723ca482d
James Oliver <j.oliver@destined.com>
2017-10-23 08:04:56 +1100

    added light meter basic script

---
+++ LightMeter/LightMeter.ino
@@ -0,0 +1,23 @@
+// This script was written by James Oliver 2017
+// It turns the LEDs into a meter for the LDR
+
+// declare variables for leds
+
+int led1 = 8;
+int led2 = 9;
+int led3 = 10;
+int led4 = 11;
+int led5 = 12;
+int led6 = 13;
+
+
+void setup() 
+{
+  // Setup LED pins as outputs
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
