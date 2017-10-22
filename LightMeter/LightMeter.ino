ab58c07decef0b5cefe20ce9922b3ffcc0c61cc3
James Oliver <j.oliver@destined.com>
2017-10-23 08:20:47 +1100

    completed light meter script

--- LightMeter/LightMeter.ino
+++ LightMeter/LightMeter.ino
@@ -14,10 +14,95 @@ int led6 = 13;
 void setup() 
 {
   // Setup LED pins as outputs
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+
+  //start serial connection so we can later print LDR value
+  Serial.begin(115200);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  // Read LDR value
+  int sensorValue = analogRead(A4);
+
+  // Print LDR value to serial monitor
+  Serial.println(sensorValue);
+
+  //determine number of LEDs on
+  int numberLedOn = map(sensorValue, 50, 260, 0, 6);
+
+  // Print Led number on to serial for testing
+  //Serial.println(numberLedOn);
+
+  //Use if else chain to turn the correct number of LEDs on
+  if(numberLedOn == 0)
+  {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);      
+  }
+  else if(numberLedOn == 1)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);      
+  }
+  if(numberLedOn == 2)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);      
+  }
+  if(numberLedOn == 3)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);      
+  }
+  if(numberLedOn == 4)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);      
+  }
+  if(numberLedOn == 5)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, LOW);      
+  }
+  if(numberLedOn == 6)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, HIGH);      
+  }
 
 }
